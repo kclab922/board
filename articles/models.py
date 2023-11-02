f080fec2af0b00be28e9afa69cda5f932dad6b73
HaeunChoi <kclab922@gmail.com>
2023-11-02 10:35:46 +0900

    모델링 / 마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Model이라는 클래스를 상속받아옴
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
